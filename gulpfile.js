c721dbcaec9b6403d918c5f8d64b93a377b357ec
Rogério Gasi <rtgasi@gmail.com>
2018-05-30 11:53:41 -0300

    Deploy script that uploads to each gas project

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,63 @@
+const fs = require('fs');
+const path = require('path');
 const gulp = require('gulp');
 const ts = require('gulp-typescript');
 const concat = require('gulp-concat');
 const run = require('gulp-run-command').default
 var runSequence = require('run-sequence');
+var spawn = require('child_process').spawn;
+
+const isWindows = process.platform === "win32";
 
 let tsProject = ts.createProject('tsconfig.json');
+let claspPath = 'build/'
+let claspFolders = [];
+
+function getFolders(dir) {
+  return fs.readdirSync(dir)
+    .filter(function(file) {
+      return fs.statSync(path.join(dir, file)).isDirectory();
+    });
+}
+
 
 gulp.task('compile', function () {
   var tsResult = gulp.src('src/**/*.ts')
     .pipe(tsProject());
 
-  return tsResult.js.pipe(gulp.dest('tmp'));
+  return tsResult.js.pipe(gulp.dest('./'));
 });
-
-gulp.task('merge', function () {
-  return gulp.src('./tmp/*.js')
-    .pipe(concat('main.js'))
-    .pipe(gulp.dest('./build/'));
-})
 
 // NOTE: Invoke help task
-gulp.task('upload', function(){
-  run('clasp push');
+gulp.task('upload', function(done){
+  let cmd = 'clasp';
+  if(isWindows) cmd += '.cmd';
+
+  finishedCount = 0;
+
+  var tasks = claspFolders.map(function(folder){
+    return spawn(cmd, ['push'], {cwd: claspPath + folder + '/'})
+      .on('close', function(){console.log('finished folder '+folder)})
+  })
+  console.log('gonna close upload');
+  done();
 });
+
+gulp.task('get-folders', function(){
+  claspFolders = getFolders(claspPath);
+  console.log(claspFolders);
+})
+
+gulp.task('copy', function(){
+  var tasks = claspFolders.map(function(folder){
+    return gulp.src(claspPath + 'main.js')
+      .pipe(gulp.dest(claspPath + folder + '/'))
+  }) 
+})
 
 gulp.task('deploy', function(){
   runSequence('compile',
-              'merge',
+              'get-folders',
+              'copy',
               'upload')
 });
